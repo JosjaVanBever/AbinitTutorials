a96e3845d1b3d2e602ddde678adb9dd423d79ab8
Josja Van Bever <josja.vanbever@gmail.com>
2019-12-06 16:57:23 +0100

    Running abinit on cluster; problem with hmem, setted up and trying for lemaitre3 and nic4

--- CaTiO3/MinCell/SP/T3/job.sh
+++ CaTiO3/MinCell/SP/T3/job.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-#SBATCH --ntasks=1
+#SBATCH --ntasks=1           # -n 
 #SBATCH --mem-per-cpu=1G
+#SBATCH --nodes=1            # -N
 
-abinit < sp.files &> log
+#SBATCH --job-name=abinit    # -J
+#SBATCH --cpus-per-task=3    # -c
+
+# REMARK: these variables are overwritten when
+#         specifying them as flags in input
+
+# load appropriate modules
+source abimodule
+
+ml list
+
+# module load openmpi/1.5.3/intel-12.0.0.084
+
+# abinit < "${INFILE}" &> log  # WORKS
+# mpirun -n "${SLURM_JOB_CPUS_PER_NODE}" abinit < "${INFILE}" &> log  # FAILS
+# mpirun -np "${SLURM_JOB_CPUS_PER_NODE}" abinit -inp "${INFILE}" &> log
+mpirun --bind-to none -n "${SLURM_JOB_CPUS_PER_NODE}" abinit < "${INFILE}" &> log
 
 exit 0
