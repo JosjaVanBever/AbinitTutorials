2196815cddf64a16ca9b7821ce49d61c1a3c9b07
Josja Van Bever <josja.vanbever@gmail.com>
2019-12-13 17:06:03 +0100

    try to run geometry optimization calculations on the CECI

--- CaTiO3/MinCell/SP/T3/job.sh
+++ CaTiO3/MinCell/SP/T3/job.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-#SBATCH --ntasks=1           # -n 
+#SBATCH --ntasks=3           # -n 
 #SBATCH --mem-per-cpu=1G
-#SBATCH --nodes=1            # -N
 
 #SBATCH --job-name=abinit    # -J
-#SBATCH --cpus-per-task=3    # -c
+#SBATCH --cpus-per-task=1    # -c
 
 # REMARK: these variables are overwritten when
 #         specifying them as flags in input
+# Other interesting variables:
+#     --nodes -N : exclusive mode; use a whole node
+
 
 # load appropriate modules
+source /CECI/home/ucl/naps/jovbever/.bashrc N
 source abimodule
+# source /CECI/home/ucl/naps/jovbever/.local/bin/abimodule
+# export PATH=/CECI/home/ucl/naps/jovbever/localHome/abinit/build/src/98_main/:$PATH
+# export PATH=$CECIHOME/localHome/abinit/build/src/98_main/:$PATH
 
+# print modules and environment for debugging
+env
 ml list
 
-# module load openmpi/1.5.3/intel-12.0.0.084
+echo $PATH
+echo `which abinit`
 
 # abinit < "${INFILE}" &> log  # WORKS
 # mpirun -n "${SLURM_JOB_CPUS_PER_NODE}" abinit < "${INFILE}" &> log  # FAILS
 # mpirun -np "${SLURM_JOB_CPUS_PER_NODE}" abinit -inp "${INFILE}" &> log
-mpirun --bind-to none -n "${SLURM_JOB_CPUS_PER_NODE}" abinit < "${INFILE}" &> log
+
+# mpirun --bind-to none -n "${SLURM_JOB_CPUS_PER_NODE}" abinit < "${INFILE}" &> log
+# mpirun --bind-to none abinit < "${INFILE}"
+mpirun -n "${SLURM_NTASKS}" abinit < "${INFILE}" &> log
 
 exit 0
